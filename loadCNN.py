15c4637bd8ce0b32056de7897a22adde4d4e4f26
Regis Rufino Rodrigues <regis.rfnrodrigues@gmail.com>
2018-04-17 01:01:52 -0300

    first commit

---
+++ loadCNN.py
@@ -0,0 +1,25 @@
+from keras.models import model_from_json
+
+from keras.preprocessing.image import ImageDataGenerator
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+
+# evaluate loaded model on test data
+# Compiling the CNN
+loaded_model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+score = loaded_model.evaluate_generator(test_set)
+print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
