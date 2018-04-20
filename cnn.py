1d37d6301dceb61feb06c0a6cb5fce39e7781f82
Regis Rufino Rodrigues <regis.rfnrodrigues@gmail.com>
2018-04-20 00:23:23 -0300

    Added another hidden layer

--- cnn.py
+++ cnn.py
@@ -23,13 +23,17 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Step 1 - Convolution
-classifier.add(Convolution2D(128, (5,5), input_shape = (64,64, 3), activation = 'relu'))
+classifier.add(Convolution2D(128, (3,3), input_shape = (64,64, 3), activation = 'relu'))
 
 # Step 2 - Pooling
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
 # Adding a second convolutional layer
-classifier.add(Convolution2D(128, (5,5), activation = 'relu'))
+classifier.add(Convolution2D(128, (3,3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Adding a second convolutional layer
+classifier.add(Convolution2D(128, (3,3), activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
 # Step 3 - Flattening
@@ -50,7 +54,7 @@ train_datagen = ImageDataGenerator(rescale = 1./255,shear_range = 0.2,zoom_range
 
 training_set = train_datagen.flow_from_directory('dataset/training_set',target_size = (64,64),batch_size = 128,class_mode = 'binary')
 
-classifier.fit_generator(training_set,steps_per_epoch = 250,epochs = 100,verbose = 1)
+classifier.fit_generator(training_set,steps_per_epoch = 250,epochs = 1,verbose = 1)
 
 # serialize model to JSON
 model_json = classifier.to_json()
